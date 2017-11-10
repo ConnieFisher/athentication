7e288f668bfc2f08f09ebcd29a4029607ee86838
ConnieFisher <ConnieFisher@users.noreply.github.com>
2017-11-10 16:05:56 -0500

    part 3

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,24 @@
-const express = require ('express');
+
 const bodyParser = require ('body-parser');
 const cors = require('cors');
+const path = 3000;
+preflight = require('preflight');
+const mongoose = require('mongoose');
+const express = require ('express');
+const app = express();
+
+app.options('*', cors());
+//app.use(cors());
+
+const posts = [
+    {message: 'hello'},
+    {message: 'hi'},
+];
+
+app.use(cors());
+
+app.get('/posts', cors(), (req,res) => {
+    res.send(posts);
+});
+
+app.listen(path);
