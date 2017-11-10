2329197e9453b8940e236cd1e16cca303548271a
ConnieFisher <ConnieFisher@users.noreply.github.com>
2017-11-10 16:57:24 -0500

    Revert "part 5"
    This reverts commit 5d69097154514227e33bdcc57ee8dafdad9307cd.

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,9 @@ preflight = require('preflight');
 const mongoose = require('mongoose');
 const express = require ('express');
 const app = express();
-const config = require('./config/database');
-const User = require('./models/User');
-
-mongoose.Promise = Promise;
 
 app.options('*', cors());
 //app.use(cors());
-
-mongoose.connect(config.database, { useMongoClient: true });
-
-mongoose.connection.on('connected', () => {
-    console.log('connected to database ' +config.database);
-  });
-  
-  //on error
-  mongoose.connection.on('error', (err) => {
-    console.log('error to database ' +err);
-  });
 
 const posts = [
     {message: 'hello'},
@@ -31,28 +16,9 @@ const posts = [
 ];
 
 app.use(cors());
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json());
-
 
 app.get('/posts', cors(), (req,res) => {
     res.send(posts);
 });
-
-app.post('/register', cors(), (req,res) => {
-    const userData = req.body;
-    const user = new User(userData)
-    // console.log(userData.email);
-
-    user.save((err, result) =>{
-        if(err)
-        console.log('connected to mongo')
-    })
-    res.sendStatus(200);
-});
-
 
 app.listen(path);
