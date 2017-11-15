060519fb3e3fb4fe017a21c5529d7f062bc47b0f
ConnieFisher <ConnieFisher@users.noreply.github.com>
2017-11-15 11:03:57 -0500

    added mongo database

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,20 @@ preflight = require('preflight');
 const mongoose = require('mongoose');
 const express = require ('express');
 const app = express();
+const config = require('./config/database')
+
+//Connect to Database
+mongoose.connect(config.database, { useMongoClient: true });
+
+//On Connection
+mongoose.connection.on('connected', () => {
+  console.log('connected to database ' +config.database);
+});
+
+//on error
+mongoose.connection.on('error', (err) => {
+  console.log('error to database ' +err);
+});
 
 app.options('*', cors());
 //app.use(cors());
