ee4a00818f12cebc68b8c4e71c77c378a0d93c40
ConnieFisher <ConnieFisher@users.noreply.github.com>
2017-11-17 08:45:49 -0500

    commiting changes already

--- backend/server.js
+++ backend/server.js
@@ -1,120 +1,128 @@
-const bodyParser = require('body-parser');
+const express = require ('express');
 const cors = require('cors');
-const path = 3000;
-preflight = require('preflight');
+port =(3000);
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const config = require('./config/database');
-const User = require('./models/Users');
+const Users = require('./models/Users');
+const preflight = require('preflight');
+const bcrypt = require('bcrypt');
 const jwt = require('jwt-simple');
-const bcrypt = require('bcryptjs');
 
 
 mongoose.Promise = Promise;
 
-//Connect to Database
-mongoose.connect(config.database, {
-    useMongoClient: true
+var app = express();
+
+
+//connection to mongoose
+const config = require('./config/database');
+mongoose.connect(config.database,{ useMongoClient: true }, (err) =>{
+    if(!err){
+        console.log('connected to mongo');
+    }else{
+        console.log('error in connecting' + err);
+    }
+
 });
 
-//On Connection
-mongoose.connection.on('connected', () => {
-    console.log('connected to database ' + config.database);
-});
+let posts = [
+    {message: 'hello'},
+    {message: 'hi'},
+]
 
-//on error
-mongoose.connection.on('error', (err) => {
-    console.log('error to database ' + err);
-});
-
-// parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-
-// parse application/json 
-app.use(bodyParser.json())
-
-app.options('*', cors());
-//app.use(cors());
-
-
-const posts = [{
-        message: 'hello'
-    },
-    {
-        message: 'hi'
-    },
-];
-
+//preflight for cors()
+//app.options('*', cors());
+//cors middleware
 app.use(cors());
+app.use(preflight());
+//bodyparser middleware
+//app.use(bodyParser.text());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//routing
 app.get('/posts', cors(), (req, res) => {
     res.send(posts);
-});
+})
 
-app.get('/users', async(req, res) => {
-    try {
-        const users = await User.find({}, '-pwd -__v');
+//routing
+app.get('/users', cors(), async(req, res) => {
+
+        const users = await User.find({}, '-pwd -__v')
         res.send(users);
-
-    } catch (error) {
+   
         console.error(error);
-        res.sendStatus(500);
+        res.sendStatus(500).send('Internal Error')
+    });
+ 
 
-    }
-    const users = await User.find({}, '-pwd -__v');
-    res.send(users);
-});
 
-app.get('/profile/:id', async(req, res) => {
-    console.log(req.params.id);
+//routing
+app.get('/profile/:id', cors(), async(req, res) => {
     try {
-        const user = await User.findById(req.params.id, '-pwd -__v');
+        var user = await User.findById(req.params.id, '-pwd -__v');
         res.send(user);
-
     } catch (error) {
         console.error(error);
-        res.sendStatus(500);
-
+        res.sendStatus(500).send('Internal Error');
     }
-    const users = await User.find({}, '-pwd -__v');
-    res.send(users);
-});
+ 
+})
+app.post('/post', (req,res) => {
+    var postData = req.body
+    postData.author = '59a365c26da2112b202b002e'
 
-app.post('/register', cors(), (req, res) => {
-    const userData = req.body;
+    var post = new Post(postData)
 
-    const user = new User(userData);
-    // console.log(userData.email);
+    post.save((err, result) => {
+        if (err) {
+            console.error('saving post error')
+            return res.status(500).send({message: 'saving post error'})
+        }
+
+        res.sendStatus(200)
+    })
+})
+
+app.post('/register',cors(), (req, res) => {
+    var userData = req.body
+
+    var user = new User(userData)
 
     user.save((err, result) => {
-        if (err) console.log('saving error');
-        res.sendStatus(200);
-    });
+        if (err)
+            console.log('saving user error')
 
+        res.sendStatus(200)
+    })
 });
-
-app.post('/login', cors(), async(req, res) => {
+    
+    
+    
+app.post('/login', cors(), async (req, res) =>{
     const loginData = req.body;
 
-    const user = await User.findOne({
-        email: loginData.email
-    });
+    const user = await User.findOne({email: loginData.email});
 
-    bcrypt.compare(loginData.pwd, user.pwd, (err, isMatch) => {
-        if (!isMatch)
-            return res.status(401).send({
-                message: 'Email or Password invalid'
-            });
+    bcrypt.compare(loginData.pwd, user.pwd, (err, isMatch) =>{
+        if(!isMatch) 
+        return res.status(401).send({message: 'Email or Password invalid'});
 
         const payload = {};
         const token = jwt.encode(payload, config.secret);
-        res.status(200).send({
-            token
-        });
+        res.status(200).send({token});
     });
 })
 
-app.listen(path);
+    
+
+//Set Static Files
+//app.use(express.static(path.join(__dirname,'public')));
+
+
+//Listen to port 3000
+app.listen(port, () => {
+    console.log(`Starting the server at port ${port}`);
+});
